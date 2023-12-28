8391636814311a2f18444d214024fdbf6ac11e57
Crowdbotics <team@crowdbotics.com>
2023-12-28 14:35:30 +0000

    Initial commit

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "lingering-sun-45218",
+  url: "https://lingering-sun-45218.botics.co",
+  api: "https://lingering-sun-45218.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
